945ce08ced0f96d10f7b126000da3b0e6cbeb5f6
JinnySarita <6432181821@student.chula.ac.th>
2023-12-09 19:30:13 +0700

    delete comment

--- PongScratch.srcs/sources_1/new/GUI.v
+++ PongScratch.srcs/sources_1/new/GUI.v
@@ -39,50 +39,43 @@ module GUI(
     input [7:0] score_2,
     output wire [11:0] rgb
     ); 
-    wire displayLeftPaddle; // to display player 1's paddle in vga
-    wire displayRightPaddle; // to display player 2's paddle in vga
-    wire displayBall; // to display ball in vga
+    wire displayLeftPaddle; 
+    wire displayRightPaddle;
+    wire displayBall; 
     
     wire displayPlayer1Score;
-    wire player1FirstDigit; // output player 1's first digit from convertor
-    wire player1SecondDigit; // output player 1's second digit from convertor
+    wire player1FirstDigit; 
+    wire player1SecondDigit; 
     wire[11:0] rgbPlayer1Score;
     
     wire displayPlayer2Score;
-    wire player2FirstDigit; // output player 1's first digit from convertor
-    wire player2SecondDigit; // output player 1's second digit from convertor
+    wire player2FirstDigit; 
+    wire player2SecondDigit; 
     wire[11:0] rgbPlayer2Score;
     
     wire[11:0] rgbLeftPaddle;
     wire[11:0] rgbRightPaddle;
     wire[11:0] rgbBall;
-    // Mux to display
     wire[5:0] outputMux;
     
-    // RGB buffer
     reg[11:0] rgbReg; 
     wire[11:0] rgbNext;
 
-    // display left paddle object on the screen
     assign displayLeftPaddle = y < paddle_1_y & y > paddle_1_y - paddle_height & x > paddle_1_x & x < paddle_1_x + paddle_width ? 1'b1 : 1'b0; 
-    assign rgbLeftPaddle = sw ? 12'b111111001001 : 12'b000000110110; // Use dark mode color if sw is 1, otherwise use light mode color
+    assign rgbLeftPaddle = sw ? 12'b111111001001 : 12'b000000110110; 
 
-    // display right paddle object on the screen
     assign displayRightPaddle = y < paddle_2_y & y > paddle_2_y - paddle_height & x > paddle_2_x & x < paddle_2_x + paddle_width ? 1'b1 : 1'b0; 
-    assign rgbRightPaddle = sw ? 12'b100111001011 : 12'b011000110100; // Use dark mode color if sw is 1, otherwise use light mode color
+    assign rgbRightPaddle = sw ? 12'b100111001011 : 12'b011000110100; 
 
-    // display ball object on the screen
     assign displayBall = (x >= ball_x - ball_rad) && (x <= ball_x + ball_rad) && (y >= ball_y - ball_rad) && (y <= ball_y + ball_rad) ? 1'b1 : 1'b0;
-    assign rgbBall = sw ? 12'b100010110100 : 12'b011101001011; // Use dark mode color if sw is 1, otherwise use light mode color
+    assign rgbBall = sw ? 12'b100010110100 : 12'b011101001011; 
 
-    // display player 1 score on the screen
     assign displayPlayer1Score = x >= 204 & x < 220 & y >= 100 & y < 108; 
     numberToPixel player1FirstDigitConvertor(score_1[7:4], y - 100, x - 204, player1FirstDigit);
     numberToPixel player1SecondDigitConvertor(score_1[3:0], y - 100, x - 212, player1SecondDigit);
     assign rgbPlayer1Score = x >= 212 ? (sw ? (player1SecondDigit ? 12'b111111111111 : 12'b000000000000) : (player1SecondDigit ? 12'b000000000000 : 12'b111111111111))
                                   : (sw ? (player1FirstDigit ? 12'b111111111111 : 12'b000000000000) : (player1FirstDigit ? 12'b000000000000 : 12'b111111111111));
 
-    // display player 2 score on the screen
     assign displayPlayer2Score = x >= 420 & x < 436 & y >= 100 & y < 108; 
     numberToPixel player2FirstDigitConvertor(score_2[7:4], y - 100, x - 420, player2FirstDigit);
     numberToPixel player2SecondDigitConvertor(score_2[3:0], y - 100, x - 428, player2SecondDigit);
@@ -94,10 +87,7 @@ module GUI(
         rgbReg <= rgbNext;   
     end
 
-    // mux
     assign outputMux = {videoOn, displayLeftPaddle, displayRightPaddle, displayBall, displayPlayer1Score, displayPlayer2Score}; 
-
-    // assign rgbNext from outputMux.
     
     assign rgbNext = outputMux === 6'b100000 ? ( sw ? 12'b000000000000 : 12'b111111111111): 
                     outputMux === 6'b110000 ? rgbLeftPaddle: 
@@ -110,10 +100,7 @@ module GUI(
                     outputMux === 6'b100010 ? rgbPlayer1Score: 
                     outputMux === 6'b100001 ? rgbPlayer2Score:
                     (sw ? 12'b1 : 12'b0);
-//                    12'd0;
-//                    sw ? 12'b000000000000 : 12'b111111111111;
  
-    // output part
     assign rgb = rgbReg; 
 
 
