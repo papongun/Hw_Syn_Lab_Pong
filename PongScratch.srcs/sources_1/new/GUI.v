0e257f2b525e2d448d3cbff2b4c350709d5033d8
JinnySarita <6432181821@student.chula.ac.th>
2023-12-09 18:57:08 +0700

    FINAL!!!!

--- PongScratch.srcs/sources_1/new/GUI.v
+++ PongScratch.srcs/sources_1/new/GUI.v
@@ -53,6 +53,9 @@ module GUI(
     wire player2SecondDigit; // output player 1's second digit from convertor
     wire[11:0] rgbPlayer2Score;
     
+    wire[11:0] rgbLeftPaddle;
+    wire[11:0] rgbRightPaddle;
+    wire[11:0] rgbBall;
     // Mux to display
     wire[5:0] outputMux;
     
@@ -62,29 +65,29 @@ module GUI(
 
     // display left paddle object on the screen
     assign displayLeftPaddle = y < paddle_1_y & y > paddle_1_y - paddle_height & x > paddle_1_x & x < paddle_1_x + paddle_width ? 1'b1 : 1'b0; 
-    assign rgbLeftPaddle = sw ? 12'b111111111111 : 12'b111010101010; // Use dark mode color if sw is 1, otherwise use light mode color
+    assign rgbLeftPaddle = sw ? 12'b111111001001 : 12'b000000110110; // Use dark mode color if sw is 1, otherwise use light mode color
 
     // display right paddle object on the screen
     assign displayRightPaddle = y < paddle_2_y & y > paddle_2_y - paddle_height & x > paddle_2_x & x < paddle_2_x + paddle_width ? 1'b1 : 1'b0; 
-    assign rgbRightPaddle = sw ? 12'b111111111111 : 12'b111010101010; // Use dark mode color if sw is 1, otherwise use light mode color
+    assign rgbRightPaddle = sw ? 12'b100111001011 : 12'b011000110100; // Use dark mode color if sw is 1, otherwise use light mode color
 
     // display ball object on the screen
-    assign displayBall = (x - ball_x) * (x - ball_x) + (y - ball_y) * (y - ball_y) <= ball_rad * ball_rad ? 1'b1 : 1'b0; 
-    assign rgbBall = sw ? 12'b111111111111 : 12'b000000000000; // Use dark mode color if sw is 1, otherwise use light mode color
+    assign displayBall = (x >= ball_x - ball_rad) && (x <= ball_x + ball_rad) && (y >= ball_y - ball_rad) && (y <= ball_y + ball_rad) ? 1'b1 : 1'b0;
+    assign rgbBall = sw ? 12'b100010110100 : 12'b011101001011; // Use dark mode color if sw is 1, otherwise use light mode color
 
     // display player 1 score on the screen
     assign displayPlayer1Score = x >= 204 & x < 220 & y >= 100 & y < 108; 
     numberToPixel player1FirstDigitConvertor(score_1[7:4], y - 100, x - 204, player1FirstDigit);
     numberToPixel player1SecondDigitConvertor(score_1[3:0], y - 100, x - 212, player1SecondDigit);
-    assign rgbPlayer1Score = x >= 212 ? player1SecondDigit ? 12'b111111111111 : 12'b000000000000
-                                  : player1FirstDigit ? 12'b111111111111 : 12'b000000000000; // color of score: white if that area contain number
+    assign rgbPlayer1Score = x >= 212 ? (sw ? (player1SecondDigit ? 12'b111111111111 : 12'b000000000000) : (player1SecondDigit ? 12'b000000000000 : 12'b111111111111))
+                                  : (sw ? (player1FirstDigit ? 12'b111111111111 : 12'b000000000000) : (player1FirstDigit ? 12'b000000000000 : 12'b111111111111));
 
     // display player 2 score on the screen
     assign displayPlayer2Score = x >= 420 & x < 436 & y >= 100 & y < 108; 
     numberToPixel player2FirstDigitConvertor(score_2[7:4], y - 100, x - 420, player2FirstDigit);
     numberToPixel player2SecondDigitConvertor(score_2[3:0], y - 100, x - 428, player2SecondDigit);
-    assign rgbPlayer2Score = x >= 428 ? player2SecondDigit ? 12'b111010101010 : 12'b000000000000
-                                   : player2FirstDigit ? 12'b111010101010 : 12'b000000000000; // color of score: white if that area contain number
+    assign rgbPlayer2Score = x >= 428 ? (sw ? (player2SecondDigit ? 12'b111111111111 : 12'b000000000000) : (player2SecondDigit ? 12'b000000000000 : 12'b111111111111))
+                                  : (sw ? (player2FirstDigit ? 12'b111111111111 : 12'b000000000000) : (player2FirstDigit ? 12'b000000000000 : 12'b111111111111));
 
 
     always @(posedge clk) begin 
